4e5b4b9d906930837d7e02716e215e3fca09b2a9
Omar Razi <startfresh87@gmail.com>
2022-01-18 12:51:40 -0500

    got api object done

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Dom Content Loaded");
+
+  const api = {
+    key: "dbe67cf3c78d1447de866b9e98991a97",
+    api: "http://api.weatherstack.com/current?access_key=",
+    units: "f",
+  };
 });
